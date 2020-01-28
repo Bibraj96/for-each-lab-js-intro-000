3414d0a5049af9c1bb24611b82c0e2058b647935
Blendi Ibraj <blendi.ibraj96@gmail.com>
2020-01-28 01:50:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function iterativeLog(array) {
   array.forEach((element, index) => {
     console.log(`${index}: ${element}`)
-  })  
+  })
 }
 
 function iterate(callback) {
   var fruits = [apple, peach, pear, plum]
   fruits.forEach(callback)
-  return fruits 
+  return fruits
 }
 
 function doToArray(array, callback) {
